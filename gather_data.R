f81f8a6a67199de48d13dadf528da1ebaddf2b08
Kyle Belanger <kyleb44@hotmail.com>
2023-07-07 16:13:58 -0400

    remove old resume, update new

---
+++ gather_data.R
@@ -0,0 +1,14 @@
+library(googlesheets4)
+
+sheet_loc <- "https://docs.google.com/spreadsheets/d/1wOAuth6R5XZjoI-9tKgHGTex7mwuGyceyhitDuq3nik"
+
+gs4_deauth()
+
+position_data <- read_sheet(sheet_loc, sheet = "positions")
+skills        <- read_sheet(sheet_loc, sheet = "skills")
+contact_info  <- read_sheet(sheet_loc, sheet = "contact_info")
+text_blocks   <- read_sheet(sheet_loc, sheet = "text_block")
+
+
+
+
